ccdc60cef894b6aafa5be7d4b01b0e16618f7e33
inceptiongt <gaotong2991@gmail.com>
2022-07-19 15:47:46 +0800

    aaa-1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,8 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>PageAAAS index</h1>
+      <h1>funcA</h1>
     </div>
   );
 }
